ffa35aa3d3cbb1d6ea43fb7cefb21672550fc43f
automationhubbd24-sys <automationhubbd24@gmail.com>
2026-03-11 13:26:15 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(() => ({
+  root: "./",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
@@ -15,6 +11,15 @@ export default defineConfig(() => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
     },
   },
 }));
